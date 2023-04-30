03bd4db9941122f2f7fd25837b51f83ffc65d024
visheshsinha <visheshsinha801@gmail.com>
2023-04-30 17:18:31 +0530

    single responsibility example

--- system-design/low-level-design/solid-princioles/singleResponsibility.py
+++ system-design/low-level-design/solid-princioles/singleResponsibility.py
@@ -27,3 +27,35 @@ class Invoice:
     def saveToDB(self) -> None:
         # saves to DB
         pass
+
+
+"""
+Converting this into Single Responsibility by breaking up the responsibilities in different classes
+"""
+
+# Any calculation logic will change in this class & not affect other classes
+class InvoiceCalculator:
+    def __init__(self, marker: Marker, qty: int) -> None:
+        self.marker = marker
+        self.qty = qty
+    
+    def calulateTotal(self) -> int:
+        price = self.marker.price * self.qty
+        return price
+
+class InvoicePrinter:
+    def __init__(self, invoice: InvoiceCalculator) -> None:
+        self.invoice = invoice
+    
+    def printInvoice(self) -> None:
+        # prints the invoice
+        pass
+
+# Data Access Layer - save in DB / in file / send in MQ, anywhere
+class InvoiceDAO:
+    def __init__(self, invoice: InvoiceCalculator) -> None:
+        self.invoice = invoice
+
+    def saveToDB(self) -> None:
+        # saves to DB
+        pass
--- system-design/low-level-design/solid-princioles/solid-principles.md
+++ system-design/low-level-design/solid-princioles/solid-principles.md
@@ -1,5 +1,11 @@
 
-S - Single Responsibility Principle
+**S - Single Responsibility Principle**
 
 - A class should have only one reason to change
+- If a class has multiple responsibility and changes if any of the responsibilities logic changes, then it's not following the single responsibility principle
+- For this we break it up into each class for one responsibility , as in the change in one will not affect the other classes & hence satisfying the Single Responsibility Principle.
+
+**O - Open Close Principle**
+
+- Open for extension but closed for modification
 - 
